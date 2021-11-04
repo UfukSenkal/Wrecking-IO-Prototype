06ecab61a9740888d403ba8275fe84f663938022
UfukSenkal <67637340+UfukSenkal@users.noreply.github.com>
2021-11-04 17:31:22 +0300

    +add abstract car base

---
+++ Assets/Scripts/Car/AbstractCarBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WreckingIO.Car
+{
+    public abstract class AbstractCarBase : MonoBehaviour
+    {
+        public abstract void Move();
+    }
+}
